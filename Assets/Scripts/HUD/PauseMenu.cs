8ff068e458bc89e1fd1c7e0c2ad46e13d1585e23
ksimaster <ksimasterlord@gmail.com>
2022-12-14 16:37:24 +0300

    fix pause mainescene

--- Assets/Scripts/HUD/PauseMenu.cs
+++ Assets/Scripts/HUD/PauseMenu.cs
@@ -11,7 +11,10 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private string howToPlayScene = "HowToPlayScene";
 
     public GameObject pauseMenuUI;
-
+    private void Start()
+    {
+        Resume();
+    }
     void Update()
     {
         if (InventoryCanvas.InventoryIsOpen) return;
